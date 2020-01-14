6e80d84221961a9019ec1b85686fc246f6795775
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-01-14 13:13:38 +0100

    furby section middle

--- scroll.js
+++ scroll.js
@@ -16,6 +16,10 @@ document.addEventListener('DOMContentLoaded', function() {
   var project1 = document.getElementById('section1');
   var showType = document.getElementById('showType');
 
+  var furby = document.getElementById('furbyTxt');
+  var projectFurby = document.getElementById('sectionFurby');
+  var showFurby = document.getElementById('showFurby');
+
   var food = document.getElementById('foodTxt');
   var project2 = document.getElementById('section2');
   var showFood = document.getElementById('showFood');
@@ -41,32 +45,36 @@ document.addEventListener('DOMContentLoaded', function() {
   // var end = document.getElementById('info');
    if( -showAbout.clientHeight< top.offsetTop- window.scrollY+200){
       landing.classList.remove('invisible');
-      invisible([type, food, dSpace, dot, contact]);
+      invisible([type, food, dSpace, dot, contact, furby]);
       black([buttons[0],buttons[1], buttons[2], buttons[3],buttons[4]]);
       arrow[0].classList.remove('up', 'blue');
       arrow[0].setAttribute("href", "#end");
    } else if(-showType.clientHeight < project1.offsetTop- window.scrollY-350) {
-      invisible([landing, food, dSpace, dot, contact]);
+      invisible([landing, food, dSpace, dot, contact, furby]);
       type.classList.remove('invisible');
       black([buttons[1], buttons[2], buttons[3],buttons[4]]);
       buttons[0].style.color="#113ffa";
+    } else if(-showFurby.clientHeight < projectFurby.offsetTop- window.scrollY+250 ){
+      invisible([landing, food, dSpace, dot, contact, type]);
+      furby.classList.remove('invisible');
+
     } else if (-showFood.clientHeight < project2.offsetTop- window.scrollY-350){
-       invisible([landing, type, dSpace, dot, contact]);
+      invisible([landing, type, dSpace, dot, contact, furby]);
       food.classList.remove('invisible');
       black([buttons[0], buttons[2], buttons[3],buttons[4]]);
       buttons[1].style.color="#113ffa";
     } else if(-showDOT.clientHeight < project3.offsetTop- window.scrollY+250) {
-      invisible([landing, type, dSpace, food, contact]);
+      invisible([landing, type, dSpace, food, contact, furby]);
       dot.classList.remove('invisible');
       black([buttons[0], buttons[1], buttons[3],buttons[4]]);
       buttons[2].style.color="#113ffa";
     } else if(-showDS.clientHeight < project4.offsetTop- window.scrollY) {
-      invisible([landing, type, dot, food, contact]);
+      invisible([landing, type, dot, food, contact, furby]);
       dSpace.classList.remove('invisible');
       black([buttons[0], buttons[1], buttons[2],buttons[4]]);
       buttons[3].style.color="#113ffa";
     } else if(-showContact.clientHeight < end.offsetTop- window.scrollY){
-       invisible([landing, type, dot, food, dSpace]);
+       invisible([landing, type, dot, food, dSpace, furby]);
       contact.classList.remove('invisible');
       arrow[0].classList.add('up', 'blue');
       arrow[0].setAttribute("href", "#top");
