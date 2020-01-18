3208cb618e9e7d94ad9f378f5f1e0238772f2b95
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-01-18 18:12:01 +0100

    adjusted scroll print/Web

--- scroll.js
+++ scroll.js
@@ -63,7 +63,7 @@ document.addEventListener('DOMContentLoaded', function() {
       food.classList.remove('invisible');
       black([buttons[0], buttons[2], buttons[3],buttons[4]]);
       buttons[1].style.color="#113ffa";
-    } else if(-showDOT.clientHeight < project3.offsetTop- window.scrollY) {
+    } else if(-showDOT.clientHeight < project3.offsetTop- window.scrollY + 1550) {
       invisible([landing, type, dSpace, food, contact, furby]);
       dot.classList.remove('invisible');
       black([buttons[0], buttons[1], buttons[3],buttons[4]]);
