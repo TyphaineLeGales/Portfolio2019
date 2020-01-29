0991954bda66a7f07e1374da2547ab353bc22322
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-01-29 10:29:01 +0100

    clippath on safari

--- scroll.js
+++ scroll.js
@@ -39,64 +39,37 @@ document.addEventListener('DOMContentLoaded', function() {
   var imgSection = document.querySelectorAll("div.showImg");
   var arrow = document.getElementsByClassName('arrow');
   var buttons = document.querySelectorAll('button');
-  console.log(descriptions, buttons);
-
-
-  var project1 = document.getElementById('section1');
-  var showType = document.getElementById('showType');
-
-
-  var projectFurby = document.getElementById('sectionFurby');
-  var showFurby = document.getElementById('showFurby');
-
-
-  var project2 = document.getElementById('section2');
-  var showFood = document.getElementById('showFood');
-
-
-  var project3 = document.getElementById('section3');
-  var showDOT = document.getElementById('showDigital');
-
-
-  var project4 = document.getElementById('section4');
-  var showDS = document.getElementById('showDigital');
 
   var top = document.getElementById('top');
-  var about = document.getElementById('showAbout');
-  var landing = document.getElementById('landing');
-
-  var contact = document.getElementById('contact');
-  var showContact = document.getElementById('showContact');
   var end = document.getElementById('end');
 
 
-   if( -showAbout.clientHeight< top.offsetTop- window.scrollY+200){
+   if( -imgSection[0].clientHeight< sections[0].offsetTop- window.scrollY+200){
       nav(0, descriptions, buttons);
       arrow[0].classList.remove('up', 'blue');
       arrow[0].setAttribute("href", "#end");
-   } else if(-showType.clientHeight < project1.offsetTop- window.scrollY) {
+   } else if(-imgSection[1].clientHeight < sections[1].offsetTop- window.scrollY) {
       nav(1, descriptions, buttons);
-    } else if(-showFurby.clientHeight < projectFurby.offsetTop- window.scrollY ){
+    } else if(-imgSection[2].clientHeight < sections[2].offsetTop- window.scrollY ){
       nav(2, descriptions, buttons);
-    } else if (-showFood.clientHeight < project2.offsetTop- window.scrollY){
+    } else if (-imgSection[3].clientHeight <sections[3].offsetTop- window.scrollY){
       nav(3, descriptions, buttons);
-    } else if(-showDOT.clientHeight < project3.offsetTop- window.scrollY ) {
+    } else if(-imgSection[4].clientHeight < sections[4].offsetTop- window.scrollY ) {
       nav(4, descriptions, buttons);
-    } else if(-showDS.clientHeight < project4.offsetTop- window.scrollY) {
+    } else if(-imgSection[5].clientHeight < sections[5].offsetTop- window.scrollY) {
       nav(5, descriptions, buttons);
-    } else if(-showContact.clientHeight < end.offsetTop- window.scrollY){
+    } else if(-imgSection[6].clientHeight < sections[6].offsetTop- window.scrollY){
       nav(6, descriptions, buttons);
       arrow[0].classList.add('up', 'blue');
       arrow[0].setAttribute("href", "#top");
     }
 
     var nom = document.getElementById('prenom');
-    var section1 = document.getElementById('section1');
     var maskDiv= document.getElementById('maskDivTitle1');
     var portrait= document.getElementById('p5CanvasMobile');
     portrait.style.top = `${window.scrollY/4}px`;
-    nom.style.opacity = `${window.scrollY/showAbout.clientHeight}`;
-    maskDiv.style.width = `${map_range(window.scrollY -project1.offsetTop, 0,showType.clientHeight, 100, 0 )}%`;
+    nom.style.opacity = `${window.scrollY/imgSection[0].clientHeight}`;
+    maskDiv.style.width = `${map_range(window.scrollY - sections[1].offsetTop, 0,imgSection[1].clientHeight, 100, 0 )}%`;
   });
 }, false);
 
