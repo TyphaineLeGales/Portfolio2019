e84fb8d0c974756085e8af1d9e392349f7ff34dc
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-08-25 15:17:11 +0200

    css clean up responsive

--- scroll.js
+++ scroll.js
@@ -33,25 +33,16 @@ function nav (currIndex, descriptionArray, btnArray) {
 
 
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('load_screen').style.display =" none";
 
   document.addEventListener('scroll', function(){
 
   var descriptions = document.querySelectorAll("div.description");
   var sections = document.querySelectorAll("div.section");
   var imgSection = document.querySelectorAll("div.showImg");
-  var arrow = document.getElementsByClassName('arrow');
   var buttons = document.querySelectorAll('button');
-  var masksHeader = document.querySelectorAll('div.maskDivTitle');
 
-  var top = document.getElementById('top');
-  var end = document.getElementById('end');
-
-
-   if( -imgSection[0].clientHeight< sections[0].offsetTop- window.scrollY+200){
-      nav(0, descriptions, buttons, masksHeader);
-      arrow[0].classList.remove('up', 'blue');
-      arrow[0].setAttribute("href", "#end");
+   if( -imgSection[0].clientHeight< sections[0].offsetTop- window.scrollY){
+      nav(0, descriptions, buttons);
    } else if(-imgSection[1].clientHeight < sections[1].offsetTop- window.scrollY) {
       nav(1, descriptions, buttons);
     } else if(-imgSection[2].clientHeight < sections[2].offsetTop- window.scrollY ){
@@ -64,24 +55,11 @@ document.addEventListener('DOMContentLoaded', function() {
       nav(5, descriptions, buttons);
     } else if(-imgSection[6].clientHeight < sections[6].offsetTop- window.scrollY){
       nav(6, descriptions, buttons);
-      arrow[0].classList.add('up', 'blue');
-      arrow[0].setAttribute("href", "#top");
     }
 
     var nom = document.getElementById('prenom');
     nom.style.opacity = `${window.scrollY/imgSection[0].clientHeight}`;
-    var portrait= document.getElementById('p5CanvasMobile');
-    var bio =  document.getElementById('bio');
 
-
-    if(window.scrollY > (window.innerHeight/1.5)) {
-      portrait.style.display = "block";
-      bio.style.display = "block";
-
-    } else {
-      portrait.style.display = "none";
-      bio.style.display = "none";
-    }
   });
 }, false);
 
